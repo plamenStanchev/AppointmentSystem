896c97255634bf6d1c4e58e123469cbfd59291fd
Radoslav Kunev <radoslav.kunev@lightds.de>
2021-05-27 12:38:05 +0300

    .

--- Server/AppointmentSystem.Server/Startup.cs
+++ Server/AppointmentSystem.Server/Startup.cs
@@ -1,67 +1,65 @@
 namespace AppointmentSystem.Server
 {
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
-    using AppointmentSystem.Server.Extensions;
-    using AppointmentSystem.Server.Features.Appointments.Hubs;
+	using AppointmentSystem.Server.Extensions;
+	using AppointmentSystem.Server.Features.Appointments.Hubs;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
 
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+			=> this.Configuration = configuration;
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddDatabase(this.Configuration)
-                .AddIdentity()
-                .AddAuthorizationFallback()
-                .AddJwtAuthentication(services.GetApplicationSettings(this.Configuration))
-                .AddApplicationServices()
-                .AddSwagger()
-                .AddedSignalR()
-                .AddControllers();
-        }
+		public void ConfigureServices(IServiceCollection services)
+			=> services
+				.AddDatabase(this.Configuration)
+				.AddIdentity()
+				.AddAuthorizationExtension()
+				.AddJwtAuthentication(services.GetApplicationSettings(this.Configuration))
+				.AddApplicationServices()
+				.AddSwagger()
+				.AddSignalExtension()
+				.AddControllers();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwaggerUI();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseSwaggerUI();
+			}
 
-            app.AddMapperProfiles();
+			app.AddMapperProfiles();
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting()
-                .UseCors(options =>
-                {
-                    options.WithOrigins("http://localhost")
-                        .AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader();
-                });
+			app
+				.UseRouting()
+				.UseCors(options =>
+				{
+					options.WithOrigins("http://localhost")
+						.AllowAnyOrigin()
+						.AllowAnyMethod()
+						.AllowAnyHeader();
+				});
 
-            app.UseAuthentication()
-                .UseAuthorization();
+			app
+				.UseAuthentication()
+				.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<NotifyHub>("/notify");
-                endpoints.MapControllers();
-            });
-            app.ApplyMigrations();
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapHub<NotifyHub>("/notify");
+				endpoints.MapControllers();
+			});
+
+			app.ApplyMigrations();
+		}
+	}
 }
