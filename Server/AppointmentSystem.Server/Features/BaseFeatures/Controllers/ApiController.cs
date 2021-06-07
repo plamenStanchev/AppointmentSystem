890f84682a499eb28c24e4d7a3fc260851f43186
Rado-K <heheheheto@gmail.com>
2021-06-07 18:09:22 +0300

    taken out automapper in base class

--- Server/AppointmentSystem.Server/Features/BaseFeatures/Controllers/ApiController.cs
+++ Server/AppointmentSystem.Server/Features/BaseFeatures/Controllers/ApiController.cs
@@ -1,23 +1,28 @@
 ﻿namespace AppointmentSystem.Server.Features.BaseFeatures.Controllers
 {
-	using AppointmentSystem.Infrastructure.Services;
-	using AppointmentSystem.Server.Filters;
-	using Microsoft.AspNetCore.Authorization;
-	using Microsoft.AspNetCore.Mvc;
+    using AppointmentSystem.Infrastructure.Services;
+    using AppointmentSystem.Server.Filters;
+    using AutoMapper;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
 
-	[Authorize]
-	[ApiController]
-	[ApiResponseFilter]
-	[Route("api/[controller]")]
-	public abstract class ApiController : ControllerBase
-	{
-		protected const string Comma = ",";
+    [Authorize]
+    [ApiController]
+    [ApiResponseFilter]
+    [Route("api/[controller]")]
+    public abstract class ApiController : ControllerBase
+    {
+        protected const string Comma = ",";
 
-		protected virtual ActionResult<Result> GenerateResultResponse(Result result)
-			=> result.Succeeded switch
-			{
-				true => this.Ok(result),
-				_ => this.BadRequest(result.Error)
-			};
-	}
+        protected IMapper UseMapper => HttpContext.RequestServices.GetRequiredService<IMapper>();
+
+        protected virtual ActionResult<Result> GenerateResultResponse(Result result)
+            => result.Succeeded switch
+            {
+                true => this.Ok(result),
+                _ => this.BadRequest(result.Error)
+            };
+    }
 }
